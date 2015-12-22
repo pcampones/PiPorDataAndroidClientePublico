8c3721239593cccbc448ebf1561188408b1063ea
Diana Jesus <2120346@my.ipleiria.pt>
2015-12-22 18:19:50 +0000

    outravezarroz

---
+++ app/src/main/java/com/example/pedrocampons/pipordatawebserviceandroidpublico/MetodosAnoActivity.java
@@ -0,0 +1,13 @@
+package com.example.pedrocampons.pipordatawebserviceandroidpublico;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MetodosAnoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_metodos_ano);
+    }
+}
